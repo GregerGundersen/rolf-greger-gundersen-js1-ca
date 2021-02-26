e00014a616a3b677aafb95cc2cb9d67e48db8c12
GregerGundersen <gregergundersen@gmail.com>
2021-02-26 12:24:26 +0100

    .

---
+++ js/details.js
@@ -0,0 +1,24 @@
+const detailContainer = document.querySelector(".chardetails");
+const queryString = document.location.search;
+const param = new URLSearchParams(queryString);
+const id = param.get("id");
+
+const url = "https://the-one-api.dev/v2/character/" + id;
+
+fetch(url, {
+  method: "GET",
+  headers: {
+    Authorization: "Bearer p7jSSrgxvJUIFJ4J0qJy",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => createHtml(data))
+  .catch((error) => console.log(error));
+
+function createHtml(details) {
+  detailContainer.innerHTML = ` <h1>${details.docs[0].name}</h1>
+                                <p>${details.docs[0].birth} - ${details.docs[0].death}</p>
+                                <b>${details.docs[0].race}</b>`;
+}
+
+// createHtml(data.docs)
--- js/script.js
+++ js/script.js
@@ -11,54 +11,24 @@ fetch("https://the-one-api.dev/v2/character", {
 const container = document.querySelector(".characters");
 
 function listChar(character) {
-  let name;
-  let race;
   let gender;
-  let link;
   let born;
   let died;
   // console.log(character[1]);
   container.innerHTML = "";
 
   for (i = 0; i < 200; i++) {
-    name = "";
-    if (character[i].name) {
-      name = character[i].name;
+    if (character[i].birth && character[i].death) {
+      if (character[i].gender === "Female") {
+        gender = '<i class="fas fa-venus"></i>';
+      } else if (character[i].gender === "Male") {
+        gender = '<i class="fas fa-mars"></i>';
+      }
     }
-    race = "";
-    if (character[i].race) {
-      race = character[i].race;
-    }
-    gender = "";
-    if (character[i].gender === "Female") {
-      gender = '<i class="fas fa-venus"></i>';
-    } else if (character[i].gender === "Male") {
-      gender = '<i class="fas fa-mars"></i>';
-    } else {
-      gender = "";
-    }
-    link = "";
-    if (character[i].wikiUrl) {
-      console.log(character[i].wikiUrl);
-      link = character[i].wikiUrl;
-    }
-    born = "Unknown DoB";
-    if (character[i].birth) {
-      birth = character[i].birth;
-    }
-
-    died = "Unknown DoD";
-    if (character[i].death) {
-      died = character[i].death;
-    }
-
-    container.innerHTML += `<div class="charcard">
-                              <h2>${name} ${gender}</h2>
-                              <h4>${born} - ${died}</h4>
-                              <div class="flex">
-                                <b>${race}</b>
-                                <a class="wikilink" href="${link}">Wiki - ${name}</a>
-                              </div>  
-                            </div>`;
+    container.innerHTML += `<a href="details.html?id=${character[i]._id}" class="charcard">
+                              <h2>${character[i].name} ${gender}</h2>
+                              <h4>${character[i].birth} - ${character[i].death}</h4>
+                              <b>${character[i].race}</b>
+                            </a>`;
   }
 }
